b717f779eaeb4ad62555997d718af389216142b5
toastoffee <931391442@qq.com>
2024-06-30 14:34:10 +0800

    add rml2016a dataset

---
+++ dataloaders/rml2016a.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+import scipy.io as scio
+
+dataset_path = "../datasets/RML2016.10a_total_data.mat"
+
+
+class RML2016aDataset(Dataset):
+    def __init__(self):
+        dataset_dict = scio.loadmat(dataset_path)
+        self.data = torch.from_numpy(dataset_dict['data'])
+        self.labels = torch.from_numpy(dataset_dict['label'])
+        self.snr = torch.from_numpy(dataset_dict['snr'])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item], self.labels[item], self.snr[item]
+
+
+if __name__ == "__main__":
+    dataset = RML2016aDataset()
